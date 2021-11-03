11975d176e37138073749230fe90544f7d2864f2
Yakovenko22V <freemailtag@gmail.com>
2021-11-03 14:05:10 +0200

    создан файл array-utils.js с функциями сортировки по возрастанию и по убыванию чисел

---
+++ array-utils.js
@@ -0,0 +1,11 @@
+const functionOne = (arr) => {
+    let arrNew = [];
+    arrNew = arr.concat()
+    return arrNew = arrNew.sort((a, b) => a - b)
+};
+
+const functionTwo = (arr) => {
+    let arrNew = [];
+    arrNew = arr.concat()
+    return arrNew = arrNew.sort((a, b) => b - a)
+};
